1f69811a1c331d8a1323fe8fc678c8af0edc63fa
leesohyeon <vkfkswjs26@gmail.com>
2024-08-24 22:38:43 +0900

    init commit

---
+++ src/main/java/com/bowmeow/common/jwt/JWTUtils.java
@@ -0,0 +1,49 @@
+package com.bowmeow.common.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JWTUtils {
+    private String secretKey = "jwt-secret-key"; // 보통 라이브러리화를 할때 SpringBoot를 포함하지 않음 >> 즉, @Value 사용불가능하나 어떻게 할지 의논 필요
+    private final Key key;
+    private final long expirationTime;
+
+    public JWTUtils(String secretKey, long expirationTime) {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        this.expirationTime = expirationTime;
+    }
+
+    public String generateToken(String userId) {
+        return Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUserId(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
